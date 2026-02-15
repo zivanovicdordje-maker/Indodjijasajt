ddafad1baa869daece404aa111e870cf44b0ad53
zivanovicdordje <zivanovicdordje@gmail.com>
2026-02-15 15:41:56 +0100

    Fix homepage

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Indodjijasajt/', // <- OBAVEZNO dodaj ovo
+      base: '/Indodjija/', // <- OBAVEZNO dodaj ovo
       server: {
         port: 3000,
         host: '0.0.0.0',
