967659d40fc7c4ea82aac5df2a1f29a2e259708d
zivanovicdordje <zivanovicdordje@gmail.com>
2026-02-15 01:29:28 +0100

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Indodjijasajt/'
-})
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // Define __dirname for ES modules environment
 const __filename = fileURLToPath(import.meta.url);
